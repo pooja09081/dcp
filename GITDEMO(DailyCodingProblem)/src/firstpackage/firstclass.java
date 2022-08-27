5c43c31100fca50e04a0842a2c617d03d991915f
pooja09081 <110563263+pooja09081@users.noreply.github.com>
2022-08-27 22:56:22 +0530

    Add files via upload

---
+++ GITDEMO(DailyCodingProblem)/src/firstpackage/firstclass.java
@@ -0,0 +1,11 @@
+package firstpackage;
+public class firstclass 
+{
+	public static void main (String args[])
+	{
+		String Intro = "  This is the first github project ";
+		System.out.println(Intro);
+		
+	}
+
+}
